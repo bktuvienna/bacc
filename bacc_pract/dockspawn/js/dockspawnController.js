a32a188b8de0460ab746adb8c99b2ca9efbe18be
benjie <benjamin.kowatsch@gmail.com>
2014-09-01 14:19:51 +0200

    navigation

--- bacc_pract/dockspawn/js/dockspawnController.js
+++ bacc_pract/dockspawn/js/dockspawnController.js
@@ -1,8 +1,11 @@
 window.onload = function() {
+
+	//converting a div to a dock manager object
 	var divDockManager = document.getElementById('dockManager');
 	var dockManager = new dockspawn.DockManager(divDockManager);
 	dockManager.initialize();
 	
+	//dock manager element should fill the entire screen
 	var onResized = function(e)
 		{
 			dockManager.resize(window.innerWidth - (divDockManager.clientLeft + divDockManager.offsetLeft), window.innerHeight - (divDockManager.clientTop + divDockManager.offsetTop));
@@ -10,10 +13,12 @@ window.onload = function() {
 	window.onresize = onResized;
 	onResized(null);
 	
+	//convert divs to dock spawn panels
 	var properties = new dockspawn.PanelContainer(document.getElementById('properties'),dockManager);
 	var code = new dockspawn.PanelContainer(document.getElementById('code'),dockManager);
 	var output = new dockspawn.PanelContainer(document.getElementById('output'),dockManager);
 	
+	//docking panels to each other
 	var documentNode = dockManager.context.model.documentManagerNode;
 	var codeNode = dockManager.dockFill(documentNode, code)
 	var propertiesNode = dockManager.dockLeft(codeNode, properties,0.15);
