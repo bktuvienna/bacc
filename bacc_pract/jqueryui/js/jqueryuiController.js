e6a5388a43eb7861cd395177dba403f2265c315a
benjie <benjamin.kowatsch@gmail.com>
2014-09-12 13:55:19 +0200

    jquery ui layout basics

--- bacc_pract/jqueryui/js/jqueryuiController.js
+++ bacc_pract/jqueryui/js/jqueryuiController.js
@@ -1,8 +1,31 @@
+var myLayout;
+
 $(document).ready(function(){
-	$('.panel').draggable({
+	$('#container').height($(document).height()-'10em');
+	/*$('.panel').draggable({
 		snap:true,
 		containment:'left',
 		containment:'center',
 		containment:'right'
-	});
-})
+	});*/
+	myLayout = $('#container').layout({
+		// some pane-size settings
+		 west__minSize: 100
+		, east__size: 300
+		, east__minSize: 200
+		, east__maxSize: .5 // 50% of layout width
+		, center__minWidth: 100
+		// some pane animation settings
+		, west__animatePaneSizing: false
+		, west__fxSpeed_size: "fast" // 'fast' animation when resizing west-pane
+		, west__fxSpeed_open: 1000 // 1-second animation when opening west-pane
+		, west__fxSettings_open: { easing: "easeOutBounce" } // 'bounce' effect when opening
+		, west__fxName_close: "none" // NO animation when closing west-pane
+		// enable showOverflow on west-pane so CSS popups will overlap north pane
+		, west__showOverflowOnHover: true
+		, showDebugMessages: true
+		});
+});
+
+
+
