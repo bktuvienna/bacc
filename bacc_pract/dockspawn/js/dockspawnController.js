bf020adc0a04c45eb9994c6f2a756b46a25e6bc2
benjie <benjamin.kowatsch@gmail.com>
2014-09-17 13:02:29 +0200

    more sophisticated scatterplot

--- bacc_pract/dockspawn/js/dockspawnController.js
+++ bacc_pract/dockspawn/js/dockspawnController.js
@@ -1,19 +1,17 @@
-//Source for D3 scatterplot(scatterplot): http://alignedleft.com/tutorials/d3/making-a-scatterplot
+//Sources for D3 scatterplot(scatterplot): http://alignedleft.com/tutorials/d3/making-a-scatterplot, http://bl.ocks.org/weiglemc/6185069
 var dataset = [
-                  [ 5,     20 ],
-                  [ 480,   90 ],
-                  [ 250,   50 ],
-                  [ 100,   33 ],
-                  [ 330,   95 ],
-                  [ 410,   12 ],
-                  [ 475,   44 ],
-                  [ 25,    67 ],
-                  [ 85,    21 ],
-                  [ 220,   88 ]
+                  [ 50,     50 ],
+                  [ 90,    80 ],
+                  [ 190,  130 ],
+                  [ 260,  180 ],
+                  [ 310,  160 ],
+                  [ 390,  210 ],
+                  [ 460,  250 ],
+                  [ 500,  280 ],
+                  [ 560,  310 ],
+                  [ 670,  350 ]
               ];
 
-var w = 500;
-var h = 100;	  
 
 window.onload = function() {
 	
@@ -37,11 +35,14 @@ window.onload = function() {
 	
 	//docking panels to each other
 	var documentNode = dockManager.context.model.documentManagerNode;
-	var scatterplotNode = dockManager.dockFill(documentNode, scatterplot)
-	var valuesNode = dockManager.dockLeft(scatterplotNode, values,0.25);
+	var scatterplotNode = dockManager.dockFill(documentNode, scatterplot)	
+	var valuesNode = dockManager.dockLeft(scatterplotNode, values,0.15);
+	var consoleNode = dockManager.dockDown(documentNode,console,0.30);
 	var outputNode = dockManager.dockRight(scatterplotNode,output,0.15);
-	var consoleNode = dockManager.dockDown(scatterplotNode,console,0.30);
-	
+
+	//updating dataset depending on input values
+	var w = $('#scatterplot').width()-50;
+	var h = $('#scatterplot').height()-50;
 	$('.values').change(function(){
 		var tmp = [];
 		$('.values').each(function(){
@@ -57,17 +58,62 @@ window.onload = function() {
 			dataset.push(data);
 		}
 		$('#vis').empty();
-		refreshScatterPlot();		
-	});	
-	refreshScatterPlot();
+		refreshScatterPlot(w,h);		
+	});
+	
+	//workaround for panels under the visualization (resizing of svg element in vis element)
+	$(window).mouseup(function(){
+		$('#vis').empty();
+		w = $('#scatterplot').width()-50;
+		h = $('#scatterplot').height()-50;
+		refreshScatterPlot(w,h);
+	});
+	
+	refreshScatterPlot(w,h);
 }
 
-var refreshScatterPlot = function(){
+//updates scatterplot and draws initial scatterplot
+var refreshScatterPlot = function(w,h){
+	
+	var xValue = function(d){return d[0];},
+		xScale = d3.scale.linear().range([0,w]),
+		xMap = function(d){return xScale(xValue(d));},
+		xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+		
+	var yValue = function(d){return d[1];},
+		yScale = d3.scale.linear().range([h,0]),
+		yMap = function(d){return yScale(yValue(d));},
+		yAxis = d3.svg.axis().scale(yScale).orient("left");
+	
+	var cValue = function(d) { return d[0];}, color = d3.scale.category10();
+	
 	var svg = d3.select("#vis")
 	.append("svg")
-	.attr("width",w)
-	.attr("height",h);
+	.attr("width",w + 50)
+	.attr("height",h + 50);
 	
+	svg.append('g')
+	.attr("class","x axis")
+	.attr("transform","translate(0," + h +")")
+	.call(xAxis)
+	.append("text")
+	.attr("class","label")
+	.attr("x", w)
+	.attr("y",-6)
+	.style("text-anchor","end")
+	.text("X AXIS");
+	
+	svg.append('g')
+	.attr("class","y axis")
+	.call(yAxis)
+	.append("text")
+	.attr("class","label")
+	.attr("transform","rotate(-90)")
+	.attr("y",6)
+	.attr("dy","0.71em")
+	.style("text-anchor","end")
+	.text("Y AXIS");
+		
 	svg.selectAll("circle")
 	.data(dataset)
 	.enter()
@@ -78,6 +124,10 @@ var refreshScatterPlot = function(){
 	.attr("cy", function(d) {
 		return d[1];
 	})
-	.attr("r", 5);
+	.attr("r", 5)
+	.style("fill",function(d){return color(cValue(d));})
+	.on("mouseover",function(d){
+		$('#output').append("Hovered :"+xValue(d) + "/"+yValue(d)+"<br>");
+	});
 }	
 			  
