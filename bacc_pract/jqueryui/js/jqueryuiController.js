19d753c1261b2afb5abde5382f8008d8ab58f289
benjie <benjamin.kowatsch@gmail.com>
2014-09-18 14:22:11 +0200

    setup layout of other frameworks

--- bacc_pract/jqueryui/js/jqueryuiController.js
+++ bacc_pract/jqueryui/js/jqueryuiController.js
@@ -1,30 +1,19 @@
 var myLayout;
 
 $(document).ready(function(){
-	$('#container').height($(document).height()-'10em');
-	/*$('.panel').draggable({
-		snap:true,
-		containment:'left',
-		containment:'center',
-		containment:'right'
-	});*/
+	var height=$(window).height() - $('header').height();
+	console.log(height);
+	$('#container').height(height);
+	
 	myLayout = $('#container').layout({
-		// some pane-size settings
-		 west__minSize: 100
-		, east__size: 300
-		, east__minSize: 200
-		, east__maxSize: .5 // 50% of layout width
-		, center__minWidth: 100
-		// some pane animation settings
-		, west__animatePaneSizing: false
-		, west__fxSpeed_size: "fast" // 'fast' animation when resizing west-pane
-		, west__fxSpeed_open: 1000 // 1-second animation when opening west-pane
-		, west__fxSettings_open: { easing: "easeOutBounce" } // 'bounce' effect when opening
-		, west__fxName_close: "none" // NO animation when closing west-pane
-		// enable showOverflow on west-pane so CSS popups will overlap north pane
-		, west__showOverflowOnHover: true
-		, showDebugMessages: true
-		});
+		center__paneSelector:".outer-center",
+		west__paneSelector:".outer-west",
+		center__childOptions: {
+			center__paneSelector:".inner-center",
+			east__paneSelector:".inner-east",
+			south__paneSelector:".inner-south"
+		}
+	});
 });
 
 
--- bacc_pract/wcDocker/js/wcDockerController.js
+++ bacc_pract/wcDocker/js/wcDockerController.js
@@ -4,24 +4,30 @@ $(document).ready(function() {
 		var layoutConfiguration;
 		var currentTheme = 'Default';
 		
-		myDocker.registerPanelType('Properties', function(myPanel){
-			myPanel.layout().addItem($('<div>testproperties</div>'));
-			myPanel.initSize(400,400);
+		myDocker.registerPanelType('Values', function(myPanel){
+			myPanel.layout().addItem($('<div></div>'));
+			myPanel.initSize(200,400);
 		});
 		
-		myDocker.registerPanelType('Code', function(myPanel){
-			myPanel.layout().addItem($('<div>testcode</div>'));
-			myPanel.initSize(400,400);
+		myDocker.registerPanelType('Scatterplot', function(myPanel){
+			myPanel.layout().addItem($('<div></div>'));
+			myPanel.initSize(900,100);
 		});
 		
 		myDocker.registerPanelType('Output', function(myPanel){
-			myPanel.layout().addItem($('<div>testoutput</div>'));
-			myPanel.initSize(400,400);
+			myPanel.layout().addItem($('<div></div>'));
+			myPanel.initSize(100,100);
 		});
 		
-		var panel1 = myDocker.addPanel('Code',wcDocker.DOCK_BOTTOM, false);
-		var panel2 = myDocker.addPanel('Properties',wcDocker.DOCK_LEFT, false);
+		myDocker.registerPanelType('Console', function(myPanel){
+			myPanel.layout().addItem($('<div></div>'));
+			myPanel.initSize(400,350);
+		});
+		
+		var panel1 = myDocker.addPanel('Scatterplot',wcDocker.DOCK_BOTTOM, false);
 		var panel3 = myDocker.addPanel('Output',wcDocker.DOCK_RIGHT, false);
+		var panel2 = myDocker.addPanel('Console',wcDocker.DOCK_BOTTOM, false);		
+		var panel4 = myDocker.addPanel('Values',wcDocker.DOCK_LEFT, false);
 		
 		 myDocker.basicMenu('.testMenu', [
 			{name: 'customMenu1', callback: function(key, opts, panel){alert(key);}},
