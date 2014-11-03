6f0a0950820c9f99421f6041db3f7e9967a9bd53
bk <benjamin.kowatsch@gmx.at>
2014-11-03 14:14:34 +0100

    toolwindow, menubar, docking

--- bacc_pract/java/mydoggy/src/controller/mainWindow.java
+++ bacc_pract/java/mydoggy/src/controller/mainWindow.java
@@ -8,6 +8,8 @@ import java.awt.Dimension;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
@@ -15,56 +17,58 @@ import org.noos.xing.mydoggy.ContentManager;
 import org.noos.xing.mydoggy.ContentManagerUI;
 import org.noos.xing.mydoggy.ToolWindow;
 import org.noos.xing.mydoggy.ToolWindowAnchor;
+import org.noos.xing.mydoggy.ToolWindowTab;
+import org.noos.xing.mydoggy.ToolWindowType;
 import org.noos.xing.mydoggy.plaf.MyDoggyToolWindow;
 import org.noos.xing.mydoggy.plaf.MyDoggyToolWindowManager;
+import org.noos.xing.mydoggy.plaf.ui.content.MyDoggyMultiSplitContentManagerUI;
 import org.noos.xing.mydoggy.plaf.ui.content.MyDoggyTabbedContentManagerUI;
 
-public class mainWindow {
-	
-	
-	//public static MyDoggyToolWindowManager init(){
-		//return twm;
-	//}
-	
+public class mainWindow {	
 	public static void main(String[] args) {
 		
 		JFrame mainWindow = new JFrame("MyDoggy");
 		MyDoggyToolWindowManager twm = new MyDoggyToolWindowManager();
-		mainWindow.getContentPane().add(twm,BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		JMenu features = new JMenu("Features");
+		menuBar.add(features);
+		
+		mainWindow.setJMenuBar(menuBar);
+		mainWindow.getContentPane().add(twm);
 		mainWindow.setSize(new Dimension(800,800));
 		mainWindow.setVisible(true);
 		
+		runContentManager(twm);
+		setUpToolWindows(twm);
+	}
+	
+	public static void runContentManager(MyDoggyToolWindowManager twm){
 		ContentManager cm = twm.getContentManager();
-		ContentManagerUI cmui = new MyDoggyTabbedContentManagerUI();
-		cm.setContentManagerUI(cmui);
+		cm.setContentManagerUI(new MyDoggyMultiSplitContentManagerUI());
+		
 		JPanel testpanel1 = new JPanel();
-		testpanel1.setPreferredSize(new Dimension(200,200));
 		cm.addContent("test1","test1",null,testpanel1);
 		
 		JPanel testpanel2 = new JPanel();
-		testpanel2.setPreferredSize(new Dimension(200,200));
-		cm.addContent("test2","test2",null,testpanel2);
+		cm.addContent("test2","test2",null,testpanel2);		
+	}
+	
+	public static void setUpToolWindows(MyDoggyToolWindowManager twm){
 		
-		
-		/*
-		//ContentManager cm = twm.getContentManager();
 		Icon icon = new ImageIcon();
-		Component component = new Button();
-		//ContentManager.addContent("Tools",
-		//						  "Tools",
-		//						  null,
-		//						  component,
-		//						  "ToolWindows"
-		//	);
-		
 		ToolWindow toolwindow = twm.registerToolWindow(
 			    "Debug",        // Tool Window identifier
 			    "Debugging",    // Tool Window Title
 			    icon,           // Tool Window Icon
-			    component,      // Tool Window Component
+			    new JPanel(),      // Tool Window Component
 			    ToolWindowAnchor.LEFT // Tool Window Anchor
-			);*/
-
+			);
+		
+		toolwindow.setType(ToolWindowType.DOCKED);
+		toolwindow.setAvailable(true);
+		
 	}
+	
 
 }
