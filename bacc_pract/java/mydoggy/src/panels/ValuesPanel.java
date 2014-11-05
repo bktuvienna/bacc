98c2e0c3a631b32f39f496c1b7eccdaedad225eb
bklap <benjamin.kowatsch@gmail.com>
2014-11-05 10:56:53 +0100

    line width and shadows bar chart

--- bacc_pract/java/mydoggy/src/panels/ValuesPanel.java
+++ bacc_pract/java/mydoggy/src/panels/ValuesPanel.java
@@ -4,10 +4,18 @@ import java.awt.Component;
 
 
 
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
@@ -20,7 +28,15 @@ public class ValuesPanel extends ApplicationFrame{
 	public ValuesPanel(String title,XYSeriesCollection dataset) {
 		super(title);
 		final JFreeChart chart = ChartFactory.createXYBarChart("","X",false,"Y",dataset,PlotOrientation.HORIZONTAL,true,true,false);
+		XYPlot plot = chart.getXYPlot();
+		XYBarRenderer renderer = (XYBarRenderer)plot.getRenderer();
+		renderer.setShadowVisible(false);
+		
 		final ChartPanel panel = new ChartPanel(chart);
+		panel.setMinimumDrawHeight(10);
+	    panel.setMaximumDrawHeight(2000);
+	    panel.setMinimumDrawWidth(10);
+	    panel.setMaximumDrawWidth(2000);
 		vpanel = panel;
 	}
 	
