991f02545b6909a5b4057c637890728c7431139b
bklap <benjamin.kowatsch@gmail.com>
2014-11-12 14:40:40 +0100

    docking frames multiple tabs on click (not working)

--- bacc_pract/java/dockingframes/src/dfcontroller/MainFrame.java
+++ bacc_pract/java/dockingframes/src/dfcontroller/MainFrame.java
@@ -10,7 +10,10 @@ import views.ValuechartView;
 import views.ValuetableView;
 import model.Data;
 import bibliothek.gui.dock.common.CControl;
+import bibliothek.gui.dock.common.CGrid;
 import bibliothek.gui.dock.common.CLocation;
+import bibliothek.gui.dock.common.CWorkingArea;
+import bibliothek.gui.dock.common.DefaultMultipleCDockable;
 import bibliothek.gui.dock.common.SingleCDockable;
 
 public class MainFrame {
@@ -23,22 +26,26 @@ public class MainFrame {
 		frame.setLayout(new GridLayout(1,1));
 		frame.add(controller.getContentArea());
 		
+		//for adding multiple tabs of scatterplot CWorking Area is needed
+		final CWorkingArea work = controller.createWorkingArea("work");
+				
 		SingleCDockable scatterplot = new ScatterplotView("scatterplot","Scatterplot",null,Data.getDataset());
 		SingleCDockable valuechart = new ValuechartView("valuechart","Valuechart",null,Data.getDataset());
 		SingleCDockable valuetable = new ValuetableView("valuetable","Valuetable",null,Data.getDataset());
 		
-		controller.addDockable(valuechart);
-		controller.addDockable(scatterplot);
-		controller.addDockable(valuetable);
-			
-		valuetable.setLocation(CLocation.base().normalEast(0.2));
-		scatterplot.setLocation(CLocation.base().normalWest(0.8));
-		valuechart.setLocation(CLocation.base().normalWest(0.2));
+		CGrid grid = new CGrid(controller);
+		grid.add(0,0,0.25,1,valuechart);
+		grid.add(0.5,0,0.5,1,work);
+		grid.add(1,0,0.25,1,valuetable);
+		controller.getContentArea().deploy(grid);
 		
-		
-		valuetable.setVisible(true);
-		scatterplot.setVisible(true);
-		valuechart.setVisible(true);
+		work.add(scatterplot);
+		work.show(scatterplot);
+		DefaultMultipleCDockable editor = new DefaultMultipleCDockable(null);
+		editor.setTitleText("Editor");
+		editor.setCloseable(true);
+		work.show(editor);
+		editor.toFront();
 		
 		frame.setBounds(20,20,1024,768);
 		frame.setVisible(true);
