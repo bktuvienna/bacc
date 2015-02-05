e3d7d87b773e10a7744d04d806803f7025eedd37
bk <benjamin.kowatsch@gmx.at>
2015-02-05 17:42:07 +0100

    added rcp menu for perspectives and reset

--- bacc_pract/java/eclipsercp/src/rcpcontroller/ApplicationActionBarAdvisor.java
+++ bacc_pract/java/eclipsercp/src/rcpcontroller/ApplicationActionBarAdvisor.java
@@ -1,5 +1,11 @@
 package rcpcontroller;
 
+//adapted source from http://www.subshell.com/en/subshell/blog/article-Eclipse-RCP-Change-Your-Perspective100.html
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
@@ -14,9 +20,30 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	// them
 	// in the fill methods. This ensures that the actions aren't recreated
 	// when fillActionBars is called with FILL_PROXY.
-
+	private IWorkbenchAction openPerspectiveAction;
+	private IWorkbenchAction savePerspectiveAsAction;
+	private IWorkbenchAction resetPerspectiveAction;
+	     
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
 	}
-
+	
+	@Override
+	protected void makeActions(IWorkbenchWindow window) {
+        openPerspectiveAction = ActionFactory.OPEN_PERSPECTIVE_DIALOG.create(window);
+        register(openPerspectiveAction);
+        savePerspectiveAsAction = ActionFactory.SAVE_PERSPECTIVE.create(window);
+        register(savePerspectiveAsAction);
+        resetPerspectiveAction = ActionFactory.RESET_PERSPECTIVE.create(window);
+        register(resetPerspectiveAction);
+	}
+	     
+	@Override
+	protected void fillMenuBar(IMenuManager menuBar) {
+        MenuManager windowMenu = new MenuManager("&Window");
+        menuBar.add(windowMenu);
+        windowMenu.add(openPerspectiveAction);
+        windowMenu.add(savePerspectiveAsAction);
+        windowMenu.add(resetPerspectiveAction);
+	}
 }
